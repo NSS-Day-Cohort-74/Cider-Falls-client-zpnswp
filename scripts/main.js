68d76055b9883bd1e8facc35877b5f236a61102e
Kai Swain <kaiferseain@icloud.com>
2024-12-06 15:43:33 -0600

    Added Areas and click event

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,14 @@
 import { guestListGenerator } from "./guestList.js"
 import { serviceList } from "./servicesList.js"
+import { areasList } from "./parkAreas.js"
 
 const guestElement = document.querySelector(".guest-sidebar")
 const serviceListElement = document.querySelector("#services-message")
+const mainElement = document.querySelector("#main-container")
 const guestHTML = guestListGenerator()
 const serviceListHTML = serviceList()
+const parkAreasHTML = areasList()
 
 guestElement.innerHTML = guestHTML
 serviceListElement.innerHTML = serviceListHTML
+mainElement.innerHTML = parkAreasHTML
--- scripts/parkAreas.js
+++ scripts/parkAreas.js
@@ -1,13 +1,48 @@
 // import ParkAreas
 // Import Guests
+import { getAreas,getGuests } from "./database.js";
+
+const guests = getGuests()
+const areas = getAreas()
+
+
 // CLICK EVENT SECTION
-// WHEN AREA NAME IS CLICKED CURRENT NUMBER OF PARK GUESTS ARE DISPLAYED IN AWINDOW ALERT "THERE ARE X GUESTS IN THEAREA"
+// WHEN AREA NAME IS CLICKED CURRENT NUMBER OF PARK GUESTS ARE DISPLAYED IN A WINDOW ALERT "THERE ARE X GUESTS IN THE AREA"
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const areaClicked = clickEvent.target
+        const id = parseInt(areaClicked.dataset.id)
+        let guestCount = 0
+        if (areaClicked.dataset.type === "area") {
+            for (const guest of guests) {
+                if ( guest.parkAreaId === id ){
+                    guestCount++
+                }
+            }
+            window.alert(`There are ${guestCount} guests in this area.`)
+        }
+    }
+)
 
 
+export const areasList = () =>{
+    let areaHTML ="<section>"
+
+    for (const area of areas) {
+        areaHTML +=`<article    data-type="area"
+                                data-id="${area.id}">
+                                ${area.name}</article>` 
+    }
+    areaHTML += "</section>"
+
+    return areaHTML
+}
 
 // HTML SECTION
 // Export function Park Areas List
 // iterate through parkAreas database
-// for each park, generate an html element store state in html variable
+// for each park, generate an html element and store state in html variable
 
 // example tags <article data-id="${}"></article>
