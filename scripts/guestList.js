73d15902cbdf0533580f4b699b8f17f713da272e
Nate Z <ntzeitlin@gmail.com>
2024-12-10 09:39:24 -0600

    added guests current location

--- scripts/guestList.js
+++ scripts/guestList.js
@@ -1,13 +1,29 @@
 // generator HTML code for guestList sidebar
 
-import { getGuests } from "./database.js"
+import { getGuests, getAreas } from "./database.js"
 const guestArray = getGuests()
+const areas = getAreas()
+
+// Display the guest location name next to the guest
+// How do we access a guests' Area
+// Every guest has a parkAreaId
 
 export const guestListGenerator = () => {
     let guestHTML = "<ul>"
 
     for (const guest of guestArray) {
-        guestHTML += `<li>${guest.name}</li>`
+        const currentGuestParkAreaId = guest.parkAreaId
+        for (const area of areas) {
+            if (currentGuestParkAreaId === area.id) {
+                guestHTML += `<li>${guest.name}: (${area.name})</li>`
+            }
+        }
+
+        // grab guest.parkAreaId
+
+        // iterate through parkAreas database
+        // grab matched location
+        // should display "Guest Name (Park Location)"
     }
 
     guestHTML += "</ul>"
